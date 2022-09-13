52e5134af6156a09b09b03c6fa7b4d393652b2a5
reidMHAC <reidelkins@outlook.com>
2022-09-13 16:39:33 -0500

    settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # Declared in your environment variables
-SECRET_KEY = os.environ('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
