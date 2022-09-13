ae332eb6b50bde062a15b3547afab75e3bfbc827
reidMHAC <reidelkins@outlook.com>
2022-09-13 16:43:12 -0500

    static files

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -206,7 +206,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
