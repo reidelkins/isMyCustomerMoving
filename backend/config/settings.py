714afcf96a5c862a6d51402ae0a74b4808fcf7a5
reidMHAC <reidelkins@outlook.com>
2022-09-13 16:54:36 -0500

    allowed hosts

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "https://is-my-customer-moving.herokuapp.com"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "https://is-my-customer-moving.herokuapp.com", "https://is-my-customer-moving.vercel.app"]
 
 # Application definition
 
