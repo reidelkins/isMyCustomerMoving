34bbf373fd7762d65725492b1ba19e6d51f45477
reidMHAC <reidelkins@outlook.com>
2022-09-13 21:02:12 -0500

    testing and production constant

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,1 +1,5 @@
-export const DOMAIN = 'https://is-my-customer-moving.herokuapp.com';
+// production
+// export const DOMAIN = 'https://is-my-customer-moving.herokuapp.com';
+
+// testing
+export const DOMAIN = 'http://localhost:8000';
