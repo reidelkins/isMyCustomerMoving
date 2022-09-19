dfffbfa81f6a72c672c78fa27e0fa243cc900864
reidMHAC <reidelkins@outlook.com>
2022-09-19 11:10:24 -0500

    test

--- backend/accounts/utils.py
+++ backend/accounts/utils.py
@@ -10,12 +10,7 @@ from django.conf import settings
 
 @shared_task
 def saveClientList(row, company_id):
-    print("got here")
-    print(company_id)
     company = Company.objects.get(id=company_id)
-    
-    
-    # for _, row in reader.iterrows():
     try:
         if int(row['zip']) > 500 and int(row['zip']) < 99951:
         # if int(row['zip']) > 37770 and int(row['zip']) < 37775:
@@ -47,63 +42,117 @@ def getAllZipcodes(company):
     company_object = Company.objects.get(id=company)
     zipCode_objects = Client.objects.filter(company=company_object).values('zipCode')
     zipCodes = ZipCode.objects.filter(zipCode__in=zipCode_objects, lastUpdated__lt=datetime.today().strftime('%Y-%m-%d'))
-    getHomesForSale.delay(list(zipCodes.values('zipCode')), company)
-    getHomesForRent.delay(list(zipCodes.values('zipCode')), company)
-    getSoldHomes.delay(list(zipCodes.values('zipCode')), company)
+    for zip in list(zipCodes.values('zipCode')):
+        getHomesForSale.delay(zip, company)
+    # getHomesForSale.delay(list(zipCodes.values('zipCode')), company)
+    # getHomesForRent.delay(list(zipCodes.values('zipCode')), company)
+    # getSoldHomes.delay(list(zipCodes.values('zipCode')), company)
 
     #TODO uncomment this
     zipCodes.update(lastUpdated=datetime.today().strftime('%Y-%m-%d'))
 
 @shared_task
-def getHomesForSale(zipCodes, company):#, company, zipCode_objects):    
+def getHomesForSale(zip, company):#, company, zipCode_objects):    
     count = 0
-    print(len(zipCodes))
-    for zip in zipCodes:
-        print(zip)
-        offset = 0
-        zip = zip['zipCode']
-        moreListings = True
-        while(moreListings):
-            conn = http.client.HTTPSConnection("us-real-estate.p.rapidapi.com")
+    
+    print(zip)
+    offset = 0
+    zip = zip['zipCode']
+    moreListings = True
+    while(moreListings):
+        conn = http.client.HTTPSConnection("us-real-estate.p.rapidapi.com")
 
-            headers = {
-                'X-RapidAPI-Key': settings.RAPID_API,
-                'X-RapidAPI-Host': "us-real-estate.p.rapidapi.com"
-                }
+        headers = {
+            'X-RapidAPI-Key': settings.RAPID_API,
+            'X-RapidAPI-Host': "us-real-estate.p.rapidapi.com"
+            }
 
-            conn.request("GET", f"/v2/for-sale-by-zipcode?zipcode={zip}&offset={offset}&limit=200&sort:newest", headers=headers)
+        conn.request("GET", f"/v2/for-sale-by-zipcode?zipcode={zip}&offset={offset}&limit=200&sort:newest", headers=headers)
 
-            res = conn.getresponse()
-            data = res.read().decode("utf-8")
-            data = json.loads(data)
-            total = data['data']['home_search']['total']
-            print(f"The total amount listed for sale at {zip} is {total} and the current offset is {offset}")
-            offset += data['data']['home_search']['count']
-            print(f"The new offset is {offset}")
-            if offset >= total:
-                moreListings = False
-            data = data['data']['home_search']['results']
+        res = conn.getresponse()
+        data = res.read().decode("utf-8")
+        data = json.loads(data)
+        total = data['data']['home_search']['total']
+        print(f"The total amount listed for sale at {zip} is {total} and the current offset is {offset}")
+        offset += data['data']['home_search']['count']
+        print(f"The new offset is {offset}")
+        if offset >= total:
+            moreListings = False
+        data = data['data']['home_search']['results']
 
-            for listing in data:
-                zip_object, created = ZipCode.objects.get_or_create(zipCode = listing['location']['address']['postal_code'])
-                try:
-                    HomeListing.objects.get_or_create(
-                                zipCode= zip_object,
-                                address= listing['location']['address']['line'],
-                                status= 'For Sale',
-                                listed= listing['list_date']
-                                )
-                except:
-                    print(f"zip: {zip_object}")
-                    print(f"address: {listing['location']['address']['line']}")
+        for listing in data:
+            zip_object, created = ZipCode.objects.get_or_create(zipCode = listing['location']['address']['postal_code'])
+            try:
+                HomeListing.objects.get_or_create(
+                            zipCode= zip_object,
+                            address= listing['location']['address']['line'],
+                            status= 'For Sale',
+                            listed= listing['list_date']
+                            )
+            except:
+                print(f"zip: {zip_object}")
+                print(f"address: {listing['location']['address']['line']}")
 
-            # try:
-            #     with open(f"/Users/reidelkins/Work/isMyCustomerMoving/sep15_{count}_sale.json", "w+") as f:
-            #         count += 1
-            #         json.dump(data, f)
-            # except:
-            #     pass
+        # try:
+        #     with open(f"/Users/reidelkins/Work/isMyCustomerMoving/sep15_{count}_sale.json", "w+") as f:
+        #         count += 1
+        #         json.dump(data, f)
+        # except:
+        #     pass
     updateStatus(company, 'For Sale')
+   
+
+
+# @shared_task
+# def getHomesForSale(zipCodes, company):#, company, zipCode_objects):    
+#     count = 0
+#     print(len(zipCodes))
+#     for zip in zipCodes:
+#         print(zip)
+#         offset = 0
+#         zip = zip['zipCode']
+#         moreListings = True
+#         while(moreListings):
+#             conn = http.client.HTTPSConnection("us-real-estate.p.rapidapi.com")
+
+#             headers = {
+#                 'X-RapidAPI-Key': settings.RAPID_API,
+#                 'X-RapidAPI-Host': "us-real-estate.p.rapidapi.com"
+#                 }
+
+#             conn.request("GET", f"/v2/for-sale-by-zipcode?zipcode={zip}&offset={offset}&limit=200&sort:newest", headers=headers)
+
+#             res = conn.getresponse()
+#             data = res.read().decode("utf-8")
+#             data = json.loads(data)
+#             total = data['data']['home_search']['total']
+#             print(f"The total amount listed for sale at {zip} is {total} and the current offset is {offset}")
+#             offset += data['data']['home_search']['count']
+#             print(f"The new offset is {offset}")
+#             if offset >= total:
+#                 moreListings = False
+#             data = data['data']['home_search']['results']
+
+#             for listing in data:
+#                 zip_object, created = ZipCode.objects.get_or_create(zipCode = listing['location']['address']['postal_code'])
+#                 try:
+#                     HomeListing.objects.get_or_create(
+#                                 zipCode= zip_object,
+#                                 address= listing['location']['address']['line'],
+#                                 status= 'For Sale',
+#                                 listed= listing['list_date']
+#                                 )
+#                 except:
+#                     print(f"zip: {zip_object}")
+#                     print(f"address: {listing['location']['address']['line']}")
+
+#             # try:
+#             #     with open(f"/Users/reidelkins/Work/isMyCustomerMoving/sep15_{count}_sale.json", "w+") as f:
+#             #         count += 1
+#             #         json.dump(data, f)
+#             # except:
+#             #     pass
+#     updateStatus(company, 'For Sale')
        
 
 @shared_task
