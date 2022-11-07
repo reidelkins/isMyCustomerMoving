f259bc5b14794523a7bab6fb9c5f33b84dbb7dbc
reidMHAC <reidelkins@outlook.com>
2022-11-06 18:56:18 -0600

    settings for celery

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -279,7 +279,7 @@ CACHES = {
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
             "CONNECTION_POOL_KWARGS": {
-                "ssl_cert_reqs": None
+                "ssl_cert_reqs": "CERT_NONE"
             },
         }
     }
