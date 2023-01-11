f45401973d91b7fa243714797243b433d9ccfe9d
reidelkins <reidelkins@outlook.com>
2023-01-10 22:23:06 -0600

    running test to see how to fix the logout/refresh issue

--- frontend/src/sections/auth/login/LoginForm.js
+++ frontend/src/sections/auth/login/LoginForm.js
@@ -44,11 +44,11 @@ export default function LoginForm() {
     setShowPassword((show) => !show);
   };
 
-  useEffect(() => {
-    if (userInfo) {
-      navigate('/dashboard/customers', { replace: true });
-    }
-  }, [navigate, userInfo]);
+  // useEffect(() => {
+  //   if (userInfo) {
+  //     navigate('/dashboard/customers', { replace: true });
+  //   }
+  // }, [navigate, userInfo]);
   return (
     <FormikProvider value={formik}>
       <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
