d5823e987529b10ac7086cec05b7c86c85360c9c
reidelkins <reidelkins@outlook.com>
2023-01-11 22:14:11 -0600

    testing

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,5 +1,5 @@
 // production
-// export const DOMAIN = 'https://is-my-customer-moving.herokuapp.com';
+export const DOMAIN = 'https://is-my-customer-moving.herokuapp.com';
 
 // testing
-export const DOMAIN = 'http://localhost:8000';
+// export const DOMAIN = 'http://localhost:8000';
