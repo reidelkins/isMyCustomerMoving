7362f9b399464fb3062c55c97f5eb8367a293100
reidelkins <reidelkins@outlook.com>
2023-01-12 00:27:33 -0600

    updated req.txt

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -40,7 +40,6 @@ urlpatterns = [
     path('api/v1/payments/', include('payments.urls')),
 
     path("stripe/", include("djstripe.urls", namespace="djstripe")),
-
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
