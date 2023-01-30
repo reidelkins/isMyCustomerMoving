6570e8904e6539ad305ce614a476df098ed56824
reidelkins <reidelkins@outlook.com>
2023-01-30 17:12:37 -0600

    remove non-used urls from production

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -7,6 +7,7 @@ from rest_framework.documentation import include_docs_urls
 from rest_framework.schemas import get_schema_view
 from drf_yasg import openapi
 from django.views.generic import TemplateView
+import os
 
 admin.site.site_header = "Django BoilerPlate Admin"
 admin.site.site_title = "Django BoilerPlate Admin"
@@ -17,28 +18,36 @@ router = routers.DefaultRouter()
 
 
 schema_view = get_schema_view(title='Rest API')
+IS_HEROKU = "DYNO" in os.environ
+if IS_HEROKU:
+    urlpatterns = [
+        path('supersecretadminpagexyz/docs/', include('django.contrib.admindocs.urls')),
+        path('supersecretadminpagexyz/', admin.site.urls),
+        path('api/v1/accounts/', include('accounts.urls')),
+        path('api/v1/payments/', include('payments.urls')),
+    ]
+else:
+    urlpatterns = [
+        # Authenticated Users only.
+        path('supersecretadminpagexyz/docs/', include('django.contrib.admindocs.urls')),
+        path('supersecretadminpagexyz/', admin.site.urls),
 
-urlpatterns = [
-    # Authenticated Users only.
-    path('supersecretadminpagexyz/docs/', include('django.contrib.admindocs.urls')),
-    path('supersecretadminpagexyz/', admin.site.urls),
+        
+        path('', TemplateView.as_view(
+            template_name='index.html',
+            extra_context={'schema_url': 'openapi-schema'}
+        ), name='swagger-ui'),
+        path("api/", include(router.urls)),
+        path('api/v1/docs/', include_docs_urls(title='Rest API')),
+        path('api/v1/schema/', get_schema_view(
+            title="Django REST API",
+            description="Django REST API Intended to Integrate with React Frontend",
+            version="1.0.0"
+        ), name='openapi-schema'),
 
-
-    path('', TemplateView.as_view(
-        template_name='index.html',
-        extra_context={'schema_url': 'openapi-schema'}
-    ), name='swagger-ui'),
-    path("api/", include(router.urls)),
-    path('api/v1/docs/', include_docs_urls(title='Rest API')),
-    path('api/v1/schema/', get_schema_view(
-        title="Django REST API",
-        description="Django REST API Intended to Integrate with React Frontend",
-        version="1.0.0"
-    ), name='openapi-schema'),
-
-    path('api/v1/accounts/', include('accounts.urls')),
-    path('api/v1/payments/', include('payments.urls')),
-]
+        path('api/v1/accounts/', include('accounts.urls')),
+        path('api/v1/payments/', include('payments.urls')),
+    ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
