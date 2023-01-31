cbefafb8136dd87185176877bc0a20a28abf6b99
reidelkins <reidelkins@outlook.com>
2023-01-31 17:36:04 -0600

    if removing takes > 15 minutes

--- backend/accounts/utils.py
+++ backend/accounts/utils.py
@@ -857,15 +857,24 @@ def remove_all_serviceTitan_tags(company):
             data = f'grant_type=client_credentials&client_id={company.clientID}&client_secret={company.clientSecret}'
             response = requests.post('https://auth.servicetitan.io/connect/token', headers=headers, data=data)
             headers = {'Authorization': response.json()['access_token'], 'Content-Type': 'application/json', 'ST-App-Key': settings.ST_APP_KEY}
+            time = datetime.datetime.now()
             tagTypes = [[str(company.serviceTitanForSaleTagID)], [str(company.serviceTitanRecentlySoldTagID)]]
             for tag in tagTypes:
                 # get a list of all the servTitanIDs for the clients with one from this company
                 # clients = list(Client.objects.filter(company=company).values_list('servTitanID'))
                 clients = list(Client.objects.filter(company=company).exclude(servTitanID=None).values_list('servTitanID', flat=True))
-                iters = (len(clients) // 100) + 1
+                iters = (len(clients) // 250) + 1
                 for i in range(iters):
+                    if time < datetime.datetime.now()-datetime.timedelta(minutes=15):
+                        headers = {
+                            'Content-Type': 'application/x-www-form-urlencoded',
+                        }
+                        data = f'grant_type=client_credentials&client_id={company.clientID}&client_secret={company.clientSecret}'
+                        response = requests.post('https://auth.servicetitan.io/connect/token', headers=headers, data=data)
+                        headers = {'Authorization': response.json()['access_token'], 'Content-Type': 'application/json', 'ST-App-Key': settings.ST_APP_KEY}
+                        time = datetime.datetime.now()
                     print(i)
-                    x = clients[i*100:(i+1)*100]
+                    x = clients[i*250:(i+1)*250]
                     payload={'customerIds': x, 'tagTypeIds': tag}
                     response = requests.delete(f'https://api.servicetitan.io/crm/v2/tenant/{str(company.tenantID)}/tags', headers=headers, json=payload)
                     if response.status_code != 200:
