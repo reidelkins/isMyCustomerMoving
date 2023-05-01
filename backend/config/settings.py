978db8320afc0d00d16bb686a0655b645716ac0a
reidelkins <reidelkins@outlook.com>
2023-04-30 23:42:24 -0500

    nothing

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -20,7 +20,10 @@ from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-dotenv.load_dotenv(BASE_DIR / '.env')
+try:
+    dotenv.load_dotenv(BASE_DIR / '.env.testing')
+except:
+    dotenv.load_dotenv(BASE_DIR / '.env')
 
 IS_HEROKU = False
 IS_GITHUB = False
