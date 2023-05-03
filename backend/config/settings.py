39fc2cfd86c08d15cb642db37bdab5c2aa0ea501
reidelkins <reidelkins@outlook.com>
2023-05-03 10:12:10 -0500

    revert settings file

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -20,11 +20,11 @@ from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-try:
-    dotenv.load_dotenv(BASE_DIR / '.env.testing')
-except:
-    dotenv.load_dotenv(BASE_DIR / '.env')
-
+# try:
+#     dotenv.load_dotenv(BASE_DIR / '.env.testing')
+# except:
+#     dotenv.load_dotenv(BASE_DIR / '.env')
+dotenv.load_dotenv(BASE_DIR / '.env')
 IS_HEROKU = False
 IS_GITHUB = False
 try:
