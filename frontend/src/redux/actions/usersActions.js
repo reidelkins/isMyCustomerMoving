ff7cc302260163613e5207b37ff7d3a82cb9f42e
reidelkins <reidelkins@outlook.com>
2023-05-30 21:20:29 -0500

    remove log statement

--- frontend/src/redux/actions/usersActions.js
+++ frontend/src/redux/actions/usersActions.js
@@ -654,7 +654,6 @@ export const recentlySoldAsync = (page) => async (dispatch, getState) => {
 
 export const filterRecentlySoldAsync = (minPrice, maxPrice, minYear, maxYear, minDaysAgo, maxDaysAgo, tagFilters, city, state, zipCode) => async (dispatch, getState) => {
   try {
-    console.log(minPrice, maxPrice, minYear, maxYear, minDaysAgo, maxDaysAgo, tagFilters, city, state, zipCode)
     const reduxStore = getState();
     const {userInfo} = reduxStore.auth.userInfo;
     const config = {
