2fdbd20e31f4bb265517bd49b9e115229f30fb83
reidelkins <reidelkins@outlook.com>
2023-06-07 21:06:25 -0500

    lock react router dom version

--- frontend/src/pages/CustomerData.js
+++ frontend/src/pages/CustomerData.js
@@ -621,8 +621,7 @@ export default function CustomerData() {
                   {userInfo.status === 'admin' && (                 
                     (
                       userInfo.company.crm === 'ServiceTitan' ? (
-                        // <ServiceTitanSyncModal clientOption={userInfo.company.clientOption}/>
-                        <ServiceTitanSyncModal />
+                        <ServiceTitanSyncModal clientOption={userInfo.company.clientOption}/>
                       )
                       :
                     (
