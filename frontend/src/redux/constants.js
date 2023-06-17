a0be61a000e767637763b9ce7ee0f773b9d5bde1
reidelkins <reidelkins@outlook.com>
2023-06-16 22:14:29 -0500

    getting docker setup

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,7 +1,9 @@
-// production
-export const DOMAIN = 'https://is-my-customer-moving.herokuapp.com';
-export const URL = 'https://app.ismycustomermoving.com';
+const { REACT_APP_ENVIRONMENT } = process.env
 
-// testing
-// export const DOMAIN = 'http://localhost:8000';
-// export const URL = 'http://localhost:3000';
+const DOMAIN = REACT_APP_ENVIRONMENT === 'dev' ? 'http://localhost:8000' : 'https://is-my-customer-moving.herokuapp.com';
+const URL = REACT_APP_ENVIRONMENT === 'dev' ? 'http://localhost:3000' : 'https://app.ismycustomermoving.com';
+
+export { DOMAIN, URL };
+
+
+
