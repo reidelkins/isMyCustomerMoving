13e427374cfbbf6d959268abad52aed3a786f8b0
reidelkins <reidelkins@outlook.com>
2023-07-07 18:11:56 -0400

    remove explicit full webhook

--- backend/payments/urls.py
+++ backend/payments/urls.py
@@ -4,9 +4,6 @@ from rest_framework import routers
 router = routers.DefaultRouter()
 
 urlpatterns = [
-    path(
-        "stripe/webhook/97eb874d-ad5d-4b4b-8889-5eb7c31f83d5/",
-        include("djstripe.urls", namespace="djstripe"),
-    ),
+    path("stripe/", include("djstripe.urls", namespace="djstripe")),
     path("", include(router.urls)),
 ]
