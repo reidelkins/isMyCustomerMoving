b232555f98ba53b971f4508b04aba43017e354d9
reidelkins <reidelkins@outlook.com>
2023-07-07 17:40:43 -0400

    remove url and test

--- backend/payments/urls.py
+++ backend/payments/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     # path('save-stripe-info/', views.save_stripe_info, name='save-stripe-info'),
     # path('setup-intent/', views.setup_intent, name='setup_intent'),
     # path('publishable-key/', views.publishable_key, name='publishable-key'),
-    path("stripe/", include("djstripe.urls", namespace="djstripe")),
+    # path("stripe/", include("djstripe.urls", namespace="djstripe")),
     path("", include(router.urls)),
 ]
