d42a7682ed6224cc973d6d152f4884b6d0efd374
reidelkins <reidelkins@outlook.com>
2023-07-07 17:31:32 -0400

    update reqs

--- backend/payments/views.py
+++ backend/payments/views.py
@@ -13,86 +13,9 @@ import stripe
 stripe.api_key = settings.STRIPE_LIVE_SECRET_KEY
 
 
-# @api_view(["POST"])
-# def save_stripe_info(request):
-#     if request.method == "POST":
-#         data = request.data
-#         email = data["email"]
-#         company = data["company"]
-#         phone = data["phone"]
-#         payment_method_id = data["payment_method_id"]
-#         try:
-#             product = Product.objects.get(
-#                 tier=data["tier"], timeFrame=data["timeFrame"]
-#             )
-#         except Product.DoesNotExist:
-#             print("Product does not exist")
-#             return Response(
-#                 "Product does not exist", status=status.HTTP_400_BAD_REQUEST
-#             )
-#         extra_msg = ""
-#         customer_data = stripe.Customer.list(email=email).data
-#         if len(customer_data) == 0:
-#             customer = stripe.Customer.create(
-#                 email=email,
-#                 payment_method=payment_method_id,
-#                 name=company,
-#                 invoice_settings={"default_payment_method": payment_method_id},
-#             )
-#         else:
-#             customer = customer_data[0]
-#             extra_msg = "Customer already exists."
-
-#         trialEnd = int((datetime.now() + timedelta(days=7)).timestamp())
-#         subscription = stripe.Subscription.create(
-#             customer=customer,
-#             items=[{"price": product.pid}],
-#             trial_end=trialEnd,
-#         )
-#         comp = makeCompany(company, email, phone, customer.id)
-#         comp.product = product
-#         comp.save()
-#         if type(comp) == dict:
-#             print("company equals dict")
-#             return Response(comp, status=status.HTTP_400_BAD_REQUEST)
-#         return Response(
-#             status=status.HTTP_200_OK,
-#             data={
-#                 "message": "Success",
-#                 "data": {
-#                     "customer_id": customer.id,
-#                     "extra_msg": extra_msg,
-#                     "sub_id": subscription,
-#                 },
-#             },
-#         )
-
-
-# @api_view(["POST"])
-# def setup_intent(request):
-#     if request.method == "POST":
-#         intent = stripe.SetupIntent.create(
-#             payment_method_types=["card"],
-#         )
-#         return Response(
-#             status=status.HTTP_200_OK,
-#             data={
-#                 "message": "Success",
-#                 "data": {"client_secret": intent.client_secret},
-#             },
-#         )
-
-
-# @api_view(["GET"])
-# def publishable_key(request):
-#     if request.method == "GET":
-#         return Response(
-#             status=status.HTTP_200_OK,
-#             data={
-#                 "message": "Success",
-#                 "data": {"publishable_key": settings.STRIPE_PUBLISHABLE_KEY},
-#             },
-#         )
+@webhooks.handler_all
+def log_all_events(event: djstripe_models.Event):
+    print(f"Received Stripe event: {event}")
 
 
 # when customer/subscription created
@@ -283,3 +206,85 @@ def update_subscription(event: djstripe_models.Event):
 #     notifications.TrialExpiresSoonEmail(
 #         customer=event.customer, data={"expiry_date": expiry_date}
 #     ).send()
+
+
+# @api_view(["POST"])
+# def save_stripe_info(request):
+#     if request.method == "POST":
+#         data = request.data
+#         email = data["email"]
+#         company = data["company"]
+#         phone = data["phone"]
+#         payment_method_id = data["payment_method_id"]
+#         try:
+#             product = Product.objects.get(
+#                 tier=data["tier"], timeFrame=data["timeFrame"]
+#             )
+#         except Product.DoesNotExist:
+#             print("Product does not exist")
+#             return Response(
+#                 "Product does not exist", status=status.HTTP_400_BAD_REQUEST
+#             )
+#         extra_msg = ""
+#         customer_data = stripe.Customer.list(email=email).data
+#         if len(customer_data) == 0:
+#             customer = stripe.Customer.create(
+#                 email=email,
+#                 payment_method=payment_method_id,
+#                 name=company,
+#                 invoice_settings={"default_payment_method": payment_method_id},
+#             )
+#         else:
+#             customer = customer_data[0]
+#             extra_msg = "Customer already exists."
+
+#         trialEnd = int((datetime.now() + timedelta(days=7)).timestamp())
+#         subscription = stripe.Subscription.create(
+#             customer=customer,
+#             items=[{"price": product.pid}],
+#             trial_end=trialEnd,
+#         )
+#         comp = makeCompany(company, email, phone, customer.id)
+#         comp.product = product
+#         comp.save()
+#         if type(comp) == dict:
+#             print("company equals dict")
+#             return Response(comp, status=status.HTTP_400_BAD_REQUEST)
+#         return Response(
+#             status=status.HTTP_200_OK,
+#             data={
+#                 "message": "Success",
+#                 "data": {
+#                     "customer_id": customer.id,
+#                     "extra_msg": extra_msg,
+#                     "sub_id": subscription,
+#                 },
+#             },
+#         )
+
+
+# @api_view(["POST"])
+# def setup_intent(request):
+#     if request.method == "POST":
+#         intent = stripe.SetupIntent.create(
+#             payment_method_types=["card"],
+#         )
+#         return Response(
+#             status=status.HTTP_200_OK,
+#             data={
+#                 "message": "Success",
+#                 "data": {"client_secret": intent.client_secret},
+#             },
+#         )
+
+
+# @api_view(["GET"])
+# def publishable_key(request):
+#     if request.method == "GET":
+#         return Response(
+#             status=status.HTTP_200_OK,
+#             data={
+#                 "message": "Success",
+#                 "data": {"publishable_key": settings.STRIPE_PUBLISHABLE_KEY},
+#             },
+#         )
