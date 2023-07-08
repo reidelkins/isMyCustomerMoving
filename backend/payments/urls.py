5a4e3afc3096b4d6e0485e3cd289017cf96de612
reidelkins <reidelkins@outlook.com>
2023-07-07 20:08:58 -0400

    revert views and url completely

--- backend/payments/urls.py
+++ backend/payments/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
+from payments import views
 
 router = routers.DefaultRouter()
 
 urlpatterns = [
+    # path('save-stripe-info/', views.save_stripe_info, name='save-stripe-info'),
+    # path('setup-intent/', views.setup_intent, name='setup_intent'),
+    # path('publishable-key/', views.publishable_key, name='publishable-key'),
     path("stripe/", include("djstripe.urls", namespace="djstripe")),
     path("", include(router.urls)),
 ]
