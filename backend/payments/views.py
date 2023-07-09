ae163968e9c0569bf210ab38761dc6bc179f334e
reidelkins <reidelkins@outlook.com>
2023-07-08 21:33:42 -0500

    make new products and customers with stripe

--- backend/payments/views.py
+++ backend/payments/views.py
@@ -2,13 +2,15 @@ from django.conf import settings
 # from django.template.loader import get_template
 # from django.core.mail import send_mail
 from rest_framework import status
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-# from accounts.models import Company, CustomUser
-# from accounts.utils import make_company, create_keap_company
-# from data.utils import delete_extra_clients
-# from .models import Product
+
+from accounts.models import Company, CustomUser
+from accounts.utils import make_company, create_keap_company
+from data.utils import delete_extra_clients
+from .models import Product
 from .serializers import ProductSerializer
 
 # from datetime import datetime, timedelta
@@ -22,6 +24,7 @@ logger = logging.getLogger(__name__)
 
 
 class StripeWebhook(APIView):
+    permission_classes = [AllowAny]
     serializer_class = ProductSerializer
 
     def post(self, request, *args, **kwargs):
@@ -30,110 +33,109 @@ class StripeWebhook(APIView):
             logger.error("Received a Stripe webhook without type")
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
-        logger.info(f"Received a Stripe webhook of type {event_type}")
-        logger.info(f"Received a Stripe webhook of type {request.data}")
+        print(f"Received a Stripe webhook of type {event_type}")
         if event_type == "checkout.session.completed":
-            return Response(status=status.HTTP_200_OK)
-            # try:
-            #     logging.info("checkout session completed")
-            #     obj = request.data["data"]["object"]
-            #     phone = obj["customer_details"]["phone"]
-            #     email = obj["customer_details"]["email"]
-            #     company_name = obj["custom_fields"][0]["text"]["value"]
-            #     stripe_id = obj["customer"]
-            #     product = Product.objects.get(id=obj["subscription"])
-            #     logging.info(f"company name: {company_name}")
-            #     try:
-            #         company = Company.objects.get(
-            #             name=company_name, email=email, stripe_id=stripe_id
-            #         )
-            #         logging.info(f"company in try retrieved: {company}")
-            #         company.stripe_id = stripe_id
-            #         company.product = product
-            #         company.phone = phone
-            #         company.save()
-            #         logging.info(f"company in try updated: {company}")
-            #         create_keap_company(company.id)
-            #     except Company.DoesNotExist:
-            #         try:
-            #             company = make_company(
-            #                 company_name, email, phone, stripe_id
-            #             )
-            #             logging.info(f"company in except: {company}")
-            #             company = Company.objects.get(id=company)
-            #             company.product = product
-            #             company.save()
-            #             logging.info(f"company in except created: {company}")
-            #             create_keap_company(company.id)
-            #         except Exception as e:
-            #             logging.error(f"error: {e}")
-            #             return Response(status=status.HTTP_400_BAD_REQUEST)
+            try:
+                logging.info("checkout session completed")
+                obj = request.data["data"]["object"]
+                phone = obj["customer_details"]["phone"]
+                email = obj["customer_details"]["email"]
+                company_name = obj["custom_fields"][0]["text"]["value"]
+                stripe_id = obj["customer"]
+                subscription = stripe.Subscription.retrieve(
+                    obj["subscription"])
 
-            #     users = CustomUser.objects.filter(company=company)
-            #     for user in users:
-            #         user.is_verified = True
-            #         user.save()
-            #     delete_extra_clients(company.id)
-            #     return Response(status=status.HTTP_200_OK)
-            # except Exception as e:
-            #     logging.error(f"error: {e}")
-            #     return Response(status=status.HTTP_400_BAD_REQUEST)
+                logging.info(f"company name: {company_name}")
+                product = Product.objects.get(id=subscription["plan"]["id"])
 
-        elif event_type == "product.created":
-            return Response(status=status.HTTP_200_OK)
-            # product = request.data["data"]["object"]
-            # product_data = {
-            #     "id": product["id"],
-            #     "amount": product["amount"],
-            #     "name": product["name"],
-            #     "interval": product["interval"],
-            # }
-            # serializer = ProductSerializer(data=product_data)
-            # if serializer.is_valid():
-            #     serializer.save()
-            #     return Response(status=status.HTTP_200_OK)
-            # else:
-            #     return Response(status=status.HTTP_400_BAD_REQUEST)
+                try:
+                    company = Company.objects.get(
+                        name=company_name, email=email, stripe_id=stripe_id
+                    )
+                    logging.info(f"company in try retrieved: {company}")
+                    company.stripe_id = stripe_id
+                    company.product = product
+                    company.phone = phone
+                    company.save()
+                    logging.info(f"company in try updated: {company}")
+                    create_keap_company(company.id)
+                except Company.DoesNotExist:
+                    try:
+                        company = make_company(
+                            company_name, email, phone, stripe_id
+                        )
+                        logging.info(f"company in except: {company}")
+                        company = Company.objects.get(id=company)
+                        company.product = product
+                        company.save()
+                        logging.info(f"company in except created: {company}")
+                        create_keap_company(company.id)
+                    except Exception as e:
+                        logging.error(f"error: {e}")
+                        return Response(status=status.HTTP_400_BAD_REQUEST)
 
-        elif event_type == "customer.subscription.created":
-            return Response(status=status.HTTP_200_OK)
-            # print("subscription created")
-            # try:
-            #     obj = request.data["data"]["object"]
-            #     customer = Company.objects.get(stripe_id=obj["customer"])
+                users = CustomUser.objects.filter(company=company)
+                for user in users:
+                    user.is_verified = True
+                    user.save()
+                delete_extra_clients(company.id)
+                return Response(status=status.HTTP_200_OK)
+            except Exception as e:
+                logging.error(f"error: {e}")
+                return Response(status=status.HTTP_400_BAD_REQUEST)
 
-            #     product = Product.objects.get(
-            #         pid=obj["items"]["data"][0]["price"]["id"]
-            #     )
-            #     company.product = product
-            #     company.save()
-            #     users = CustomUser.objects.filter(company=company)
-            #     for user in users:
-            #         user.isVerified = True
-            #         user.save()
-            #     return Response(status=status.HTTP_200_OK)
-            # except Exception as e:
-            #     print(e)
-            #     print("error")
-            #     return Response(status=status.HTTP_400_BAD_REQUEST)
+        elif event_type == "price.created":
+            try:
+                Product.objects.create(
+                    id=request.data["data"]["object"]["id"],
+                    amount=(
+                        float(request.data["data"]["object"]["unit_amount_decimal"])/100),
+                    interval=request.data["data"]["object"]["recurring"]["interval"]
+                )
+                return Response(status=status.HTTP_200_OK)
+            except Exception as e:
+                logging.error(f"error: {e}")
+                return Response(status=status.HTTP_400_BAD_REQUEST)
 
-        elif event_type == "customer.subscription.updated":
-            return Response(status=status.HTTP_200_OK)
-            # try:
-            #     obj = request.data["data"]["object"]
-            #     customer_data = stripe.Customer.list(email=email).data
-            #     customer = customer_data[0]
-            #     product = Product.objects.get(id=customer.plan.id)
+        # elif event_type == "customer.subscription.created":
+        #     return Response(status=status.HTTP_200_OK)
+        #     # print("subscription created")
+        #     # try:
+        #     #     obj = request.data["data"]["object"]
+        #     #     customer = Company.objects.get(stripe_id=obj["customer"])
 
-            #     company.product = product
-            #     company.save()
-            #     if product.id != "price_1MhxfPAkLES5P4qQbu8O45xy":
-            #         delete_extra_clients(company.id)
-            # except Exception as e:
-            #     print(e)
-            #     print("error")
-        # canceled subscription
-        elif event_type == "customer.subscription.deleted":
+        #     #     product = Product.objects.get(
+        #     #         pid=obj["items"]["data"][0]["price"]["id"]
+        #     #     )
+        #     #     company.product = product
+        #     #     company.save()
+        #     #     users = CustomUser.objects.filter(company=company)
+        #     #     for user in users:
+        #     #         user.isVerified = True
+        #     #         user.save()
+        #     #     return Response(status=status.HTTP_200_OK)
+        #     # except Exception as e:
+        #     #     print(e)
+        #     #     print("error")
+        #     #     return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        # elif event_type == "customer.subscription.updated":
+        #     return Response(status=status.HTTP_200_OK)
+        #     # try:
+        #     #     obj = request.data["data"]["object"]
+        #     #     customer_data = stripe.Customer.list(email=email).data
+        #     #     customer = customer_data[0]
+        #     #     product = Product.objects.get(id=customer.plan.id)
+
+        #     #     company.product = product
+        #     #     company.save()
+        #     #     if product.id != "price_1MhxfPAkLES5P4qQbu8O45xy":
+        #     #         delete_extra_clients(company.id)
+        #     # except Exception as e:
+        #     #     print(e)
+        #     #     print("error")
+        # # canceled subscription
+        # elif event_type == "customer.subscription.deleted":
             return Response(status=status.HTTP_200_OK)
             # try:
             #     obj = request.data["data"]["object"]
