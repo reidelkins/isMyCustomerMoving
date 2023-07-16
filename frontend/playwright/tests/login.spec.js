42a1c66f107707ccb5a0007cd51684fd7a91c0ca
Jack Long <jack.richard.long@gmail.com>
2023-07-16 15:19:51 -0500

    protected routes tests + 10s timeout login test

--- frontend/playwright/tests/login.spec.js
+++ frontend/playwright/tests/login.spec.js
@@ -1,18 +1,22 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test.beforeEach(async ({ page }) => {
-  // Logout
-  await page.goto('/dashboard');
-  await page.getByTestId('profile-button').click();
-  await page.getByRole('menuitem', { name: /logout/i }).click();
-  await expect(page).toHaveURL(/.*logout/);
-
-  // Got to login page
-  await page.goto('/login');
-});
 
 test.describe('Login', () => {
+  /*
+  Logout before each login test
+  */
+  test.beforeEach(async ({ page }) => {
+    // Logout
+    await page.goto('/dashboard');
+    await page.getByTestId('profile-button').click();
+    await page.getByRole('menuitem', { name: /logout/i }).click();
+    await expect(page).toHaveURL(/.*logout/);
+  
+    // Got to login page
+    await page.goto('/login');
+  });
+
   test('login with correct email and password', async ({ page }) => {
     // Fill out the form
     const email = process.env.TEST_LOGIN_EMAIL ? process.env.TEST_LOGIN_EMAIL : '';
@@ -24,7 +28,7 @@ test.describe('Login', () => {
     await page.getByRole('button', { name: /login/i }).click();
 
     // Expects the URL to contain intro.
-    await expect(page).toHaveURL(/.*dashboard/);
+    await expect(page).toHaveURL(/.*dashboard/, { timeout: 10000 });
   });
 
   test('login with wrong password', async ({ page }) => {
--- frontend/playwright/tests/routes.spec.js
+++ frontend/playwright/tests/routes.spec.js
@@ -1,7 +1,7 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test.describe('Routes', () => {
+test.describe('Routes exist and have titles', () => {
   test('/dashboard', async ({ page }) => {
     await page.goto('/dashboard');
     await expect(page).toHaveTitle(/Home/);
@@ -30,5 +30,46 @@ test.describe('Routes', () => {
   test('/dashboard/settings/enterprise', async ({ page }) => {
     await page.goto('/dashboard/settings/enterprise');
     await expect(page).toHaveTitle(/Enterprise Settings/);
+  });
+});
+
+test.describe('Ensure routes are protected', () => {
+  /*
+  Logout before hitting protected routes
+  */
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/dashboard');
+    await page.getByTestId('profile-button').click();
+    await page.getByRole('menuitem', { name: /logout/i }).click();
+    await expect(page).toHaveURL(/.*logout/);
+  });
+
+  test('protected /dashboard', async ({ page }) => {
+    await page.goto('/dashboard');
+    await expect(page).toHaveURL(/.*login/);
+  });
+  test('protected /dashboard/customers', async ({ page }) => {
+    await page.goto('/dashboard/customers');
+    await expect(page).toHaveURL(/.*login/);
+  });
+  test('protected /dashboard/referrals', async ({ page }) => {
+    await page.goto('/dashboard/referrals');
+    await expect(page).toHaveURL(/.*login/);
+  });
+  test('protected /dashboard/forsale', async ({ page }) => {
+    await page.goto('/dashboard/forsale');
+    await expect(page).toHaveURL(/.*login/);
+  });
+  test('protected /dashboard/recentlysold', async ({ page }) => {
+    await page.goto('/dashboard/recentlysold');
+    await expect(page).toHaveURL(/.*login/);
+  });
+  test('protected /dashboard/settings/user', async ({ page }) => {
+    await page.goto('/dashboard/settings/user');
+    await expect(page).toHaveURL(/.*login/);
+  });
+  test('protected /dashboard/settings/enterprise', async ({ page }) => {
+    await page.goto('/dashboard/settings/enterprise');
+    await expect(page).toHaveURL(/.*login/);
   });
 });
