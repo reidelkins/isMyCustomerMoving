5d603dc9bf8b7d661f4018dbde9ab8b1e2919130
Jack Long <jack.richard.long@gmail.com>
2023-07-31 23:49:24 -0500

    client table pagination

--- frontend/README.md
+++ frontend/README.md
@@ -43,8 +43,8 @@ Frontend Tests 28 June 2023
 
 #### Dashboard
 
-- [ ] Show 10 per page, 50, 100 clients
-- [ ] View more than 1000 clients
+- [x] Show 10 per page, 50, 100 clients
+- [ ] View more than 1000 clients  (how should I test this?  Just click through pages?)
 - [ ] Make note
 - [ ] Set as contacted/not contacted
   - [ ] This is not updating automatically on the site. You pretty much have to refresh the page to get the clients again and then it is accurate
--- frontend/playwright/tests/dashboard.spec.js
+++ frontend/playwright/tests/dashboard.spec.js
@@ -1,34 +1,43 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
+test.describe('Pagination', () => {
+  test.beforeEach(async ({ page }) => {
+    // Add listeners for request and response events
+    // page.on('request', (request) => {console.log('>>', request.method(), request.url())});
+    // page.on('response', (response) => {console.log('<<', response.status(), response.url())});
 
-test.describe('Pagination', () => { 
-    test.beforeEach(async ({ page }) => {
-        // Add listeners for request and response events
-        // page.on('request', (request) => {console.log('>>', request.method(), request.url())});
-        // page.on('response', (response) => {console.log('<<', response.status(), response.url())});
+    await page.goto('/dashboard/customers');
+    await page.waitForLoadState('load');
 
-        await page.goto('/dashboard/customers');
-        await page.waitForLoadState('load');
+    // Wait for the first page of clients from the API
+    const responsePromise = page.waitForResponse('**/api/v1/data/clients/?page=1');
+    const response = await responsePromise;
+    expect(response.status()).toBe(200);
+  });
 
-        // Wait for the first page of clients from the API
-        const responsePromise = page.waitForResponse('**/api/v1/data/clients/?page=1');
-        await responsePromise;
-    });
+  test('default show 10 clients', async ({ page }) => {
+    const rows = page.locator('tr');
+    expect(await rows.count()).toBe(11);
+  });
 
-    test('table defaults to 10 rows', async ({ page }) => {
-        const table = page.getByTestId('customer-data-table');
+  test('show 50 clients', async ({ page }) => {
+    const rows = page.locator('tr');
 
-        const rows = await table.locator('tbody tr').count()
-        expect(rows).toBe(10);
-    });
+    // Click pagination and change to 50 rows
+    await page.getByLabel('10').click()
+    await page.getByRole('option', { name: '50' }).click()
 
-    test('change from 10 to 50 clients per page', async ({ page }) => {        
-        await page.getByRole('button', { name: '10' }).click();
-        
-        const dropDown = page.getByRole('listbox');
-        expect(dropDown).toBeVisible();
-    });
+    expect(await rows.count()).toBe(51);
+  });
 
+  test('show 100 clients', async ({page}) => {
+    const rows = page.locator('tr');
 
- })
+    // Click pagination and change to 50 rows
+    await page.getByLabel('10').click()
+    await page.getByRole('option', { name: '100' }).click()
+
+    expect(await rows.count()).toBe(101);
+  })
+});
