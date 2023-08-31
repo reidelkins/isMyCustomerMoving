d037f4146af7d7ccc10fafadb3cd4c5741ccef30
reidelkins <reidelkins@outlook.com>
2023-08-30 20:20:00 -0500

    test serpy

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -263,7 +263,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": ["accounts.utils.CustomAuthentication"],
     "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100,  # or any other default page size you prefer
+    'PAGE_SIZE': 1000,  # or any other default page size you prefer
 }
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
--- backend/data/serializers.py
+++ backend/data/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+import drf_serpy as serpy
+
 
 from accounts.serializers import EnterpriseSerializer, BasicCompanySerializer
 from .models import (
@@ -11,11 +13,21 @@ from .models import (
 )
 
 
-class ClientUpdateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ClientUpdate
-        fields = [f.name for f in ClientUpdate._meta.fields]
-        read_only_fields = fields
+# class ClientUpdateSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = ClientUpdate
+#         fields = [f.name for f in ClientUpdate._meta.fields]
+#         read_only_fields = fields
+
+class ClientUpdateSerializer(serpy.Serializer):
+    id = serpy.Field()
+    client = serpy.Field(attr='client.id')
+    date = serpy.Field()
+    status = serpy.Field()
+    listed = serpy.Field()
+    note = serpy.Field()
+    contacted = serpy.Field()
+    error_flag = serpy.Field()
 
 
 class ClientSerializer(serializers.ModelSerializer):
@@ -30,29 +42,55 @@ class ClientSerializer(serializers.ModelSerializer):
         ]
 
 
-class ClientListSerializer(serializers.ModelSerializer):
-    zip_code = serializers.CharField(source='zip_code.zip_code')
-    tag = serializers.SerializerMethodField()
-    service_titan_customer_since_year = serializers.IntegerField(default=1900)
-    client_updates_client = ClientUpdateSerializer(many=True, read_only=True)
+# class ClientListSerializer(serpy.Serializer):
+#     zip_code = serpy.Field(source='zip_code.zip_code')
+#     zip_code = serializers.CharField(source='zip_code.zip_code')
+#     tag = serializers.SerializerMethodField()
+#     service_titan_customer_since_year = serializers.IntegerField(default=1900)
+#     client_updates_client = ClientUpdateSerializer(many=True, read_only=True)
+
+#     def get_tag(self, obj):
+#         return [tag.tag for tag in obj.tag.all()]
+
+#     class Meta:
+#         model = Client
+#         fields = [
+#             "id",
+#             "name",
+#             "address",
+#             "city",
+#             "state",
+#             "phone_number",
+#             "status",
+#             "service_titan_customer_since_year",
+#             "service_titan_lifetime_revenue",
+#             "latitude",
+#             "longitude",
+#             "zip_code",
+#             "tag",
+#             "service_titan_customer_since_year",
+#             "client_updates_client"
+#         ]
+#         read_only_fields = fields
+
+class ClientListSerializer(serpy.Serializer):
+    id = serpy.Field()
+    name = serpy.Field()
+    address = serpy.Field()
+    city = serpy.Field()
+    state = serpy.Field()
+    phone_number = serpy.Field()
+    status = serpy.Field()
+    service_titan_customer_since_year = serpy.Field()
+    service_titan_lifetime_revenue = serpy.Field()
+    latitude = serpy.Field()
+    longitude = serpy.Field()
+    zip_code = serpy.Field(attr='zip_code.zip_code')
+    tag = serpy.MethodField()
+    client_updates_client = ClientUpdateSerializer(many=True)
 
     def get_tag(self, obj):
         return [tag.tag for tag in obj.tag.all()]
-
-    class Meta:
-        model = Client
-        fields = [
-            f.name
-            for f in Client._meta.fields
-            if f.name != "service_titan_customer_since"
-            and f.name != "zip_code"
-        ] + [
-            "zip_code",
-            "tag",
-            "service_titan_customer_since_year",
-            "client_updates_client",
-        ]
-        read_only_fields = fields
 
 
 class ZapierClientSerializer(serializers.ModelSerializer):
@@ -92,7 +130,7 @@ class ReferralSerializer(serializers.ModelSerializer):
     enterprise = EnterpriseSerializer(required=False)
     referred_from = BasicCompanySerializer(required=True)
     referred_to = BasicCompanySerializer(required=True)
-    client = ClientListSerializer(read_only=True)
+    client = ClientListSerializer()
 
     class Meta:
         model = Referral
