52039d492979afd706781984b32137f6877aad2e
reidelkins <reidelkins@outlook.com>
2023-08-30 20:24:54 -0500

    test serpy without client updates

--- backend/data/serializers.py
+++ backend/data/serializers.py
@@ -87,7 +87,7 @@ class ClientListSerializer(serpy.Serializer):
     longitude = serpy.Field()
     zip_code = serpy.Field(attr='zip_code.zip_code')
     tag = serpy.MethodField()
-    client_updates_client = ClientUpdateSerializer(many=True)
+    # client_updates_client = ClientUpdateSerializer(many=True)
 
     def get_tag(self, obj):
         return [tag.tag for tag in obj.tag.all()]
